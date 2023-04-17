1dd51e90c37820be72937a15d99136dfb491f910
Ruhangs <Ruhangs@163.com>
2023-04-17 17:41:09 +0800

    home首页

--- src/main.js
+++ src/main.js
@@ -1,6 +1,31 @@
 import Vue from 'vue'
 import App from './App'
 import './uni.promisify.adaptor'
+import { $http } from '@escook/request-miniprogram'
+
+uni.$http = $http
+
+// 请求的根路径
+$http.baseUrl = "https://www.uinav.com"
+
+$http.beforeRequest = function(option){
+  uni.showLoading({
+    title: '数据加载中...'
+  })
+}
+
+$http.afterRequest = function(){
+  uni.hideLoading()
+}
+
+// 封装弹窗的方法
+uni.$showMsg = function(title = '数据请求失败！', duration = 1500){
+  uni.showToast({
+    title,
+    duration,
+    icon: "none"
+  })
+}
 
 Vue.config.productionTip = false
 
